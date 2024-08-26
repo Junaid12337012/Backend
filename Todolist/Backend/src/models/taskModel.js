0e98ac4e1de8e477935f20748b9378473c14093a
junaid khan <junaidmushtaq988@gmail.com>
2024-08-26 20:25:13 +0500

    INitial commit

--- Todolist/Backend/src/models/taskModel.js
+++ Todolist/Backend/src/models/taskModel.js
@@ -2,7 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 
 const taskschema = new mongoose.Schema({
-    
+    title : {
+        type : String ,
+        required : true 
+    },
+    description : {
+        type : String ,
+        required : true 
+    },
+    completed : {
+        type : Boolean ,
+        default : false
+    },
+    createAT : {
+        type : Date ,
+        default : Date.now
+    }
 }, { timestamps : "true" });
 
 const Task = mongoose.model(Task , taskschema)
+
+export default Task;
---
+++ Todolist/Backend/src/routes/taskRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import Task from '../models/taskModel';
+
+const router = express.Router();
+
+//create new Task 
+
