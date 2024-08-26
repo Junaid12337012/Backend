4e28a4e67f6fbb4d1a4e8f66c174e5173b06ecc3
junaid khan <junaidmushtaq988@gmail.com>
2024-08-26 21:54:41 +0500

    Update router.js

--- Todolist/Backend/src/routes/taskRoutes.js
+++ Todolist/Backend/src/routes/taskRoutes.js
@@ -4,4 +4,41 @@ import Task from '../models/taskModel';
 const router = express.Router();
 
 //create new Task 
+router.post('/' , async(req , res ) => {
+    try{
+        res.status(201).json( await new Task(req.body).save() );
+    } catch (error) {
+        res.status(400).json({error : error.message});
+    }   
+});
 
+// get all Task 
+router.get('/' , async(req , res)=> {
+    try{
+        res.status(200).json( await Task.find() )
+    } catch (error) {
+        res.status(400).json({error : error.message})
+    }
+});
+
+// update Task
+router.get('/:id' , async(req , res) => {
+    try{
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new : true})
+        res.status(200).json( task );
+    } catch (error) {
+        res.status(400).json({error : error.message});
+    }
+});
+
+//delete Task
+router.delete('/:id' , async(req , res) => {
+    try{
+        await Task.findByIdAndDelete(req.params.id);
+        res.status(200).json({message : "Task Deleted"});
+    } catch (error) {
+        res.status(400).json({error : error.message})
+    }
+});
+
+export default router ;
