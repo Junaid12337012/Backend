c9f76084a71de7c0ac5750f67a1c94036fb0ef34
junaid khan <junaidmushtaq988@gmail.com>
2024-08-26 14:29:27 +0500

    Add database

--- Todolist/Backend/src/config/db.js
+++ Todolist/Backend/src/config/db.js
@@ -1,1 +1,22 @@
-    
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        });
+        console.log(`MongoDB Connected`);
+    }
+        catch (error) {
+            console.error(`Error : ${error.message}`);
+            process.exit(1);
+        }
+};
+
+export default connectDB;
